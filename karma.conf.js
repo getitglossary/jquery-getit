ed89e2911e8a522f569844ef1230c7720be5f2ca
Robin Layfield <robin@ultrasimplified.com>
2016-01-25 23:20:02 +0000

    Initial build of plugin

---
+++ karma.conf.js
@@ -0,0 +1,12 @@
+module.exports = function(config) {
+  config.set({
+    files: [
+			'node_modules/jquery/dist/jquery.js',
+            'dist/jquery.getit.min.js',
+			'test/setup.js',
+      'test/spec/*'
+    ],
+    frameworks: ['qunit'],
+		autoWatch: true
+  });
+};
